51b094e5e76b1bf9bfa3091cfda29efaaa9eb4aa
Yure Ribeiro <yureribeirocontato@gmail.com>
2024-03-03 18:12:02 -0300

    animação de linhas

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 // But this could work too
 /////
 
-const themeSwitcher = document.querySelector("[data-theme-picker]");
+// const themeSwitcher = document.querySelector("[data-theme-picker]");
 
-themeSwitcher.addEventListener("change", () => {
-  if (themeSwitcher.value === "☀️") {
-    document.documentElement.setAttribute("data-theme", "☀️");
-  }
-  if (themeSwitcher.value === "🌑") {
-    document.documentElement.setAttribute("data-theme", "🌑");
-  }
-  if (themeSwitcher.value === "💻") {
-    document.documentElement.removeAttribute("data-theme");
-  }
-});
+// themeSwitcher.addEventListener("change", () => {
+//   if (themeSwitcher.value === "☀️") {
+//     document.documentElement.setAttribute("data-theme", "☀️");
+//   }
+//   if (themeSwitcher.value === "🌑") {
+//     document.documentElement.setAttribute("data-theme", "🌑");
+//   }
+//   if (themeSwitcher.value === "💻") {
+//     document.documentElement.removeAttribute("data-theme");
+//   }
+// });
